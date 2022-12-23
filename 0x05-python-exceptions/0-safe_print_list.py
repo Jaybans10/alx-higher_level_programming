5f82476f6533d2d0438ac597e6ec8a1165b9d24c
Jaybans10 <jaybans10@yahoo.com>
2022-12-23 08:32:20 -0800

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     """Print x elememts of a list.
+
     Args:
         my_list (list): The list to print elements from.
         x (int): The number of elements of my_list to print.
+
     Returns:
         The number of elements printed.
     """
@@ -16,6 +18,4 @@ def safe_print_list(my_list=[], x=0):
         except IndexError:
             break
     print("")
-    return (ret)
-Footer
-© 2022 GitHub, Inc. 
+    return (ret) 
